a17410376047ed4ba81c5c6e0ffaf91ceffc1abb
jaozin011 <140723111+jaozin011@users.noreply.github.com>
2024-09-23 07:52:58 -0300

    Add files via upload

---
+++ 3Tri-Finalizado-main/3-Tri-main/3 Trimestre/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Artes',
+    'Quem pintou Mona Lisa?',
+    'Mona Lisa, foi pintada entre os anos 1503 e 1506 por Leonardo da Vinci (1452-1519).'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as fases da Lua?',
+    'As fases da lua são quatro: nova, crescente, cheia e minguante. Cada uma delas dura 7 ou 8 dias.'
+)
+
+criaCartao(
+    'História',
+    'Quando terminou a Segunda Guerra Mundial',
+    'A Segunda Guerra Mundial terminou em 1945'
+)
+
+criaCartao(
+    'Geografia',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'A Terra demora aproximadamente 24 horas'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual a personagem mais famosa de Maurício de Sousa?',
+    'Mônica é a personagem principal das histórias em quadrinhos da Turma da Mônica.'
+)
+
+criaCartao(
+    'Computação',
+    'O que é maior: gigabyte ou megabyte?',
+    'O gigabyte é maior do que o megabyte. Um gigabyte equivale a 1024 megabytes.'
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual a nacionalidade do sociólogo e filósofo Durkheim?',
+    'David Émile Durkheim (1858- 1917 nasceu na cidade francesa de Épinal.'
+)
